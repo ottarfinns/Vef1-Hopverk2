12e216064dfeb120daa006b35433b2e399cb2c9b
ottarfinns <ottarfinns@gmail.com>
2023-11-17 21:45:49 +0000

    Fyrsta útgáfa á route fallinu útfært, föll færð yfir í ui.js

--- src/index.js
+++ src/index.js
@@ -1,119 +1,24 @@
 import { el } from './lib/element.js';
-import { fetchFromLink } from './lib/api.js';
+import { renderProduct, renderFrontPage, navBar } from './lib/ui.js';
 
 const bodyEl = document.body;
 const wrapperEl = el('div', {
   class: 'wrapper',
 });
-const mainEl = el('main', {
-  class: 'main',
-});
-wrapperEl.appendChild(mainEl);
 bodyEl.appendChild(wrapperEl);
-
-const navBar = async () => {
-  const navEl = el(
-    'nav',
-    { class: 'nav flex justify-between' },
-    el('h1', { class: 'h1 self-center text-2xl' }, 'Vefforritunarbúðin')
-  );
-  const navDiv = el(
-    'div',
-    { class: 'nav-div flex gap-4 flex-wrap' },
-    el(
-      'div',
-      { class: 'flex gap-4' },
-      el('a', { class: 'nav-link', href: '#' }, 'Nýskrá'),
-      el('a', { class: 'nav-link', href: '#' }, 'Innskrá'),
-      el('a', { class: 'nav-link', href: '#' }, 'Karfa')
-    ),
-    el(
-      'div',
-      { class: 'flex gap-4' },
-      el('a', { class: 'nav-link', href: '#' }, 'Nýjar Vörur'),
-      el('a', { class: 'nav-link', href: '#' }, 'Flokkar')
-    )
-  );
-  navEl.appendChild(navDiv);
-  mainEl.appendChild(navEl);
-};
-navBar();
-
-async function nyjarVorur() {
-  const res = await fetchFromLink('products?limit=6');
-
-  const products = res.items;
-
-  console.log(products);
-
-  const list = el('ul', { class: 'product-list grid grid-cols-12 gap-4' });
-
-  for (const prod of products) {
-    const productElement = el(
-      'div',
-      { class: 'prod-card col-span-4' },
-      el(
-        'a',
-        { href: `/products/{${prod.id}}` },
-        el('img', { class: 'prod-img object-cover', src: `${prod.image}` })
-      ),
-      el(
-        'div',
-        { class: 'prod-info-container' },
-        el(
-          'span',
-          { class: 'prod-title' },
-          `${prod.title} ${prod.category_title}`
-        ),
-        el('span', { class: 'prod-price' }, `${prod.price} kr.-`)
-      )
-    );
-    list.appendChild(productElement);
-  }
-
-  mainEl.appendChild(list);
-}
-nyjarVorur();
-
-async function categoriesFront() {
-  const res = await fetchFromLink('/categories?limit=12');
-  const categories = res.items;
-
-  const categoryContainer = el(
-    'div',
-    { class: 'cat-container' },
-    el(
-      'h2',
-      { class: 'cat-cont-header text-xl font-bold' },
-      'Skoðaðu vöruflokkana okkar'
-    )
-  );
-
-  const list = el('ul', { class: 'category-list grid gap-4 grid-cols-12' });
-
-  for (const cat of categories) {
-    const catElement = el(
-      'div',
-      {
-        class:
-          'category border-solid border-black border-2 col-span-4 text-center text-5xl',
-      },
-      `${cat.title}`
-    );
-    list.appendChild(catElement);
-  }
-  console.log(res);
-  categoryContainer.appendChild(list);
-  mainEl.appendChild(categoryContainer);
-}
-
-categoriesFront();
+navBar(wrapperEl);
 
 function route() {
-  const { search } = window.location;
+  const { pathname } = window.location;
 
-  const sParam = new URLSearchParams(search);
+  const sParams = new URLSearchParams(pathname);
 
-  console.log(sParam);
+  const id = sParams.get('id');
+
+  if (id) {
+    renderProduct();
+  } else {
+    renderFrontPage(wrapperEl);
+  }
 }
 route();
--- src/lib/ui.js
+++ src/lib/ui.js
@@ -1,1 +1,104 @@
+import { el } from './element.js';
+import { fetchFromLink } from './api.js';
 
+export function renderProduct() {}
+
+export function navBar(parentElement) {
+  const navEl = el(
+    'nav',
+    { class: 'nav flex justify-between w-full' },
+    el('h1', { class: 'h1 self-center text-2xl' }, 'Vefforritunarbúðin')
+  );
+  const navDiv = el(
+    'div',
+    { class: 'nav-div flex gap-4 flex-wrap' },
+    el(
+      'div',
+      { class: 'flex gap-4' },
+      el('a', { class: 'nav-link', href: '#' }, 'Nýskrá'),
+      el('a', { class: 'nav-link', href: '#' }, 'Innskrá'),
+      el('a', { class: 'nav-link', href: '#' }, 'Karfa')
+    ),
+    el(
+      'div',
+      { class: 'flex gap-4' },
+      el('a', { class: 'nav-link', href: '#' }, 'Nýjar Vörur'),
+      el('a', { class: 'nav-link', href: '#' }, 'Flokkar')
+    )
+  );
+  navEl.appendChild(navDiv);
+  parentElement.appendChild(navEl);
+}
+
+async function nyjarVorur() {
+  const res = await fetchFromLink('products?limit=6');
+  const products = res.items;
+
+  const list = el('ul', { class: 'product-list grid grid-cols-12 gap-4' });
+
+  for (const prod of products) {
+    const productElement = el(
+      'div',
+      { class: 'prod-card col-span-4' },
+      el(
+        'a',
+        { href: `/products/?id=${prod.id}` },
+        el('img', { class: 'prod-img object-cover', src: `${prod.image}` })
+      ),
+      el(
+        'div',
+        { class: 'prod-info-container' },
+        el(
+          'span',
+          { class: 'prod-title' },
+          `${prod.title} ${prod.category_title}`
+        ),
+        el('span', { class: 'prod-price' }, `${prod.price} kr.-`)
+      )
+    );
+    list.appendChild(productElement);
+  }
+
+  return list;
+}
+
+async function categoriesFront() {
+  const res = await fetchFromLink('/categories?limit=12');
+  const categories = res.items;
+
+  const categoryContainer = el(
+    'div',
+    { class: 'cat-container' },
+    el(
+      'h2',
+      { class: 'cat-cont-header text-xl font-bold' },
+      'Skoðaðu vöruflokkana okkar'
+    )
+  );
+
+  const list = el('ul', { class: 'category-list grid gap-4 grid-cols-12' });
+
+  for (const cat of categories) {
+    const catElement = el(
+      'div',
+      {
+        class:
+          'category border-solid border-black border-2 col-span-4 text-center text-5xl',
+      },
+      `${cat.title}`
+    );
+    list.appendChild(catElement);
+  }
+  console.log(res);
+  categoryContainer.appendChild(list);
+  return categoryContainer;
+}
+
+export async function renderFrontPage(parentElement) {
+  const mainEl = el('main', { class: 'main' });
+  const productList = await nyjarVorur();
+  const categoryContainer = await categoriesFront();
+  mainEl.appendChild(productList);
+  mainEl.appendChild(categoryContainer);
+  parentElement.appendChild(mainEl);
+}
