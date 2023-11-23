e366d60a315aa24ab59cc792c7e646ff7535e6ea
Fridrik Olafsson <72094943+FridrikThor@users.noreply.github.com>
2023-11-23 19:43:45 +0000

    tók út komment og gamalt tailwind

--- src/index.js
+++ src/index.js
@@ -36,8 +36,6 @@ function route() {
 window.onpopstate = () => {
   const mainEl = document.querySelector('main');
   const form = document.querySelector('.form-container');
-  /* const sParams = new URLSearchParams(window.location.search);
-  const query = sParams.get('query'); */
 
   if (form) {
     form.remove();
